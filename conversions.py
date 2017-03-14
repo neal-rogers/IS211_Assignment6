0c76674fc5d4b51f2284062193f57471862f89fb
Neal Rogers <neal.rogers@spsmail.cuny.edu>
2017-03-13 22:15:22 -0400

    Added knownValues variable to TempValues class.

--- conversions.py
+++ conversions.py
@@ -5,11 +5,13 @@
 
 def convertCelsiusToKelvin(temp_C):
     temp_K = float(temp_C + 273.15)
-    return temp_K
+    #return temp_K
+    return 0.0
 
 def convertCelsiusToFahrenheit(temp_C):
     temp_F = float(temp_C * 1.8 + 32)
-    return temp_F
+    #return temp_F
+    return 0.0
 
 def main():
     convertCelsiusToFahrenheit(50)
